577603d5449a0d9b5e2c9abba38b6a57a21f6826
TAOtxi <1321075268@qq.com>
2023-05-04 17:43:00 +0800

    🐛修改ARIMA模型参数 之前输入ACF和PACF函数图的数据没有差分，现在对需要差分的数据进行差分，并修改相对应的p和q参数

--- ACF和PACF图像.py
+++ ACF和PACF图像.py
@@ -3,16 +3,21 @@ import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 data = pd.read_excel('数据文件.xls')
+
 fig = plt.figure(figsize=(15, 40))
+diff_ = [0, 1, 1, 1, 1, 1, 0, 1]
+
 for i in range(1, 9):
 
     # 绘制时间序列的ACF和PACF图
     # 图表每一行代表一个地区
-    
     ts = data.iloc[:, i]
+    if diff_[i - 1] == 1:
+        ts = ts.diff().dropna()
     ax1 = fig.add_subplot(8, 2, 2*i - 1)
     ax2 = fig.add_subplot(8, 2, 2*i)
+
     plot_acf(ts, ax=ax1)
-    plot_pacf(ts, ax=ax2)
+    plot_pacf(ts, ax=ax2, method='ywm')
 
 plt.show()

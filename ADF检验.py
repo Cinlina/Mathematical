f56890ea74d46beb838c471a24d226bb26c15fc9
TAOtxi <1321075268@qq.com>
2023-05-07 09:40:39 +0800

    🎨数据表格

--- ADF检验.py
+++ ADF检验.py
@@ -3,24 +3,25 @@ from statsmodels.tsa.stattools import adfuller
 
 data = pd.read_excel('数据文件.xls')
 
-def ADF(ts):
+index = ['ADF统计值', 'p值', '临界值(1%)', '临界值(5%)', '临界值(10%)']
+df_0 = pd.DataFrame(index=index, columns=['Region%d' % i for i in range(1, 9)])
+df_1 = pd.DataFrame(index=index, columns=['Region%d' % i for i in range(1, 9)])
 
-    index = ['ADF统计值', 'p值', '临界值(1%)', '临界值(5%)', '临界值(10%)']
-    df = pd.DataFrame(index=index, columns=['0阶差分', '1阶差分'])
-
-    result = adfuller(ts)
-    df['0阶差分'] = [result[0], result[1], result[4]['1%'], result[4]['5%'], result[4]['10%']]
-
-    ts = ts.diff().dropna()
-    result = adfuller(ts)
-    df['1阶差分'] = [result[0], result[1], result[4]['1%'], result[4]['5%'], result[4]['10%']]
-
-    return df
-
+# ADF检验
 for i in range(1, 9):
     ts = data.iloc[:, i]
-    print('='*30)
-    print('region%d:' % i)
 
     # ADF检验数据
-    print(ADF(ts))
+    # 没有差分的DataFrame
+    result = adfuller(ts)
+    df_0['Region%d' % i] = [result[0], result[1], result[4]['1%'], result[4]['5%'], result[4]['10%']]
+
+    # 进行差分的DataFrame
+    ts = ts.diff().dropna()
+    result = adfuller(ts)
+    df_1['Region%d' % i] = [result[0], result[1], result[4]['1%'], result[4]['5%'], result[4]['10%']]
+
+print('原始数据的ADF检验结果：')
+print(df_0)
+print('一阶差分后的ADF检验结果：')
+print(df_1)
--- 白噪声检验.py
+++ 白噪声检验.py
@@ -5,18 +5,21 @@ data = pd.read_excel('数据文件.xls')
 
 # diff_ 标记为1的数据需要进行一阶差分
 diff_ = [0, 1, 1, 1, 1, 1, 0, 1]
+df = pd.DataFrame(index=[i for i in range(1, 11)], columns=['Region%d' % i for i in range(1, 9)])
 
 for i in range(1, 9):
     ts = data.iloc[:, i]
     if diff_[i - 1] == 1:
         ts = ts.diff().dropna()
 
-    print('=' * 30)
-    print('region%d:' % i)
     # 进行Ljung-Box检验
     # 判断准则：
-    # 对于每一个滞后阶数，
-    # LB统计量小于选定置信水平下的临界值，或者值大于显著性水平（如0.05），不能拒绝原假设，序列为白噪声；
-    # LB统计量大于选定置信水平下的临界值，或者值小于显著性水平（如0.05），拒绝原假设，序列非白噪声；
+    # 对于每一个滞后阶数（行索引），
+    # p值大于显著性水平（如0.05），不能拒绝原假设，序列为白噪声；
+    # p值小于显著性水平（如0.05），拒绝原假设，序列非白噪声；
 
-    print(acorr_ljungbox(ts, lags=10).rename(columns={'lb_stat': 'LB统计量', 'lb_pvalue': 'p值'}))
+    result = acorr_ljungbox(ts, lags=10)
+    df['Region%d' % i] = result['lb_pvalue']
+
+df.index.name = '滞后阶数'
+print(df)
